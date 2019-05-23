344af2d0fa601105e9163a522d9c92d0c80ea973
jomulder <jomulder@gmail.com>
2019-05-23 13:32:16 +0200

    correction to the earlier importFrom statements in BFregression

--- R/BFregression.R
+++ R/BFregression.R
@@ -3,22 +3,13 @@
 ### 2019) using (m)lm-objects.
 
 #' @importFrom pracma rref
-#' @importFrom mvtnorm dmvnorm pmvnorm dmvt pmvt
+#' @importFrom mvtnorm dmvnorm pmvnorm rmvnorm dmvt pmvt
 #' @importFrom Matrix rankMatrix
+#' @importFrom MCMCpack rinvgamma
 #' @importFrom MASS ginv
 #' @method BF lm
 #' @export
 
-
-#' @importFrom Matrix rankMatrix
-#' @importFrom utils tail pmvt mvtnorm
-#' @importFrom utils tail dmvt mvtnorm
-#' @importFrom utils tail pmvnorm mvtnorm
-#' @importFrom utils tail dmvnorm mvtnorm
-#' @importFrom utils tail rmvnorm mvtnorm
-#' @importFrom utils tail rref pracma
-#' @importFrom utils tail ginv MASS
-#' @importFrom utils tail rinvgamma MCMCpack
 BF.lm <- function(x,
                   hypothesis = NULL,
                   prior = NULL,
