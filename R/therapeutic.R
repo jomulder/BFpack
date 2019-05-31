4d9255a9f40350f4f9759fb071455e1f8b0fae9f
jomulder <jomulder@gmail.com>
2019-05-31 15:05:52 +0200

    Update therapeutic.R

--- R/therapeutic.R
+++ R/therapeutic.R
@@ -2,7 +2,7 @@
 #' Data come from an experimental study (Rosa, Rosa, Sarner, and Barrett, 1998) 
 #' that were also used in Howell (2012, p.196).
 #' An experiment was conducted to investigate if Therapeutic Touch practitioners 
-#' who were blindfolded can effectively identify which of their hands is below the experimenter¡¯s.
+#' who were blindfolded can effectively identify which of their hands is below the experimenterÂ¡Â¯s.
 #' Twenty-eight practitioners were involved and tested 10 times in the experiment. 
 #' Researchers expected an average of 5 correct answers from each practitioner 
 #' as it is the number by chance if they do not outperform others.
@@ -11,5 +11,5 @@
 
 ## Load data from local source. Please set working directory where data stored.
 ## setwd(D:/...)
-ttestdata<-read.csv("Therapeutic.csv")
+ttestdata<-read.csv("therapeutic.csv")
 #ttestdata is a data frame with one column (correct answers) and 28 rows (number of practitioners).
