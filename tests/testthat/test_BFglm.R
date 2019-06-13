2d99ce44ba79587314c65be1d445991474caaca5
Unknown <anton.collentine@gmail.com>
2019-06-13 14:36:51 +0200

    Added skeleton test examples for glm, coxph and glmerMod classes

---
+++ tests/testthat/test_BFglm.R
@@ -0,0 +1,11 @@
+#Test glm class object
+library(BFpack)
+
+counts <- c(18,17,15,20,10,20,25,13,12)
+outcome <- gl(3,1,9)
+treatment <- gl(3,3)
+
+glm.D93 <- glm(counts ~ outcome + treatment, family = poisson())
+
+BF(glm.D93)
+BF.glm(glm.D93, hypothesis = "treatment2 = treatment3")
