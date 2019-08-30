de6ee748cdc9bc6828cce26bf84c7ac2513e80ba
jomulder <jomulder@gmail.com>
2019-08-30 11:22:43 +0200

    some changes to test_ttest

--- tests/testthat/test_ttest.R
+++ tests/testthat/test_ttest.R
@@ -1,25 +1,56 @@
-# # test BF for t test
-#
-# library(bain)
-# ttest1 <- t_test(therapeutic,mu=5)
-# BF1 <- BF(ttest1)
-# summary(BF1)
-# BF1 <- BF(x=ttest1,hypothesis="mu=5;mu<5")
-# BF1$BFtu_confirmatory
-# summary(BF1)
-#
-# # two samples t test with equal variances
-# ttest1 <- t_test(therapeutic,therapeutic*.7+.5,var.equal=T)
-# BF(ttest1)
-# BF1 <- BF(ttest1,hypothesis="difference=0.2;difference<0.2")
-# summary(BF1)
-#
-# # two samples t test with unequal variances
-# ttest1 <- t_test(therapeutic,therapeutic*.7+.5,"two.sided",var.equal=F)
-# BF(ttest1)
-# BF1 <- BF(ttest1,hypothesis="difference=0.2;difference<0.2")
-# summary(BF1)
-#
-#
-#
-#
+# test BF for t test
+
+#####################
+# ONE SAMPLE T TEST #
+#####################
+
+library(bain)
+
+# test if confirmatory test gives same result exploratory test
+ttest1 <- t_test(therapeutic,mu=5)
+BF1 <- BF(x=ttest1,hypothesis="mu=5;mu<5")
+expect_true(
+  all.equal(unname(c(BF1$PHP_exploratory)),unname(BF1$PHP_confirmatory))
+)
+
+# test if one-sided PMP is same as one-sided p-value
+ttest2 <- t_test(therapeutic,mu=5,alternative="less")
+BF2 <- BF(x=ttest2,hypothesis="mu>5")
+expect_equivalent(
+  ttest2$p.value,c(BF2$PHP_confirmatory)[1]
+)
+
+######################################
+# TWO SAMPLES T TEST EQUAL VARIANCES #
+######################################
+
+# check if posterior model probabilities are correct
+ttest3 <- t_test(therapeutic,therapeutic*.9+.1,var.equal=T)
+BF3 <- BF(ttest3)
+expect_equivalent(
+  c(unname(BF3$PHP_exploratory)),c(0.767913,0.04941605,0.1826709)
+  ,tolerance = .00001)
+
+# test if one-sided PMP is same as one-sided p-value
+ttest4 <- t_test(therapeutic,therapeutic*.9+.1,var.equal=T,alternative="greater")
+BF4 <- BF(ttest4,"difference<0")
+expect_equivalent(
+  ttest4$p.value,c(BF4$PHP_confirmatory)[1]
+)
+
+
+########################################
+# TWO SAMPLES T TEST UNEQUAL VARIANCES #
+########################################
+
+# check posterior probabilities for a given data set
+ttest5 <- t_test(therapeutic,therapeutic*.7+2.5,"two.sided",var.equal=F)
+set.seed(123)
+BF5 <- BF(ttest5)
+expect_equivalent(
+  c(unname(BF5$PHP_exploratory)),c(0.0607,0.9374,0.0019)
+  ,tolerance = .001)
+
+
+
+
