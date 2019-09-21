9f01cba26d27eda7b840b56a053762c56e8fbb7e
Lissa <c.j.vanlissa@uu.nl>
2019-09-21 13:10:43 +0200

    Voeg paar test_that() statements toe Voeg get_estimates.hetcor toe

--- tests/testthat/test_application1_BF.bain_ttest.R
+++ tests/testthat/test_application1_BF.bain_ttest.R
@@ -3,19 +3,19 @@
 ttest1 <- t_test(therapeutic,alternative="greater",mu=5)
 # one-sided Bayesian one sample t test
 BF1 <- BF(ttest1,hypothesis="mu<5",prior=c(.5,.5))
-expect_equivalent(
+test_that("t_test p-value equal to php", {expect_equivalent(
   round(ttest1$p.value,3),round(BF1$PHP_confirmatory[1],3)
-)
+)})
 # confirmatory Bayesian one sample t test
 BF2 <- BF(ttest1,hypothesis="mu=5;mu>5",prior=c(.5,.5,0))
 #check prior output
-expect_equivalent(
+test_that("t_test prior correct in output", {expect_equivalent(
   BF2$prior,c(.5,.5,0)
-)
+)})
 #check posterior probabilities
-expect_equivalent(
+test_that("t_test PHP correct", {expect_equivalent(
   round(BF2$PHP_confirmatory,3),c(0.943,0.057,0.000)
-)
+)})
 
 
 
