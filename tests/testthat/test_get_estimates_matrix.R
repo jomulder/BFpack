978b0a54237b6546a783afe7366bf88456338755
jomulder <jomulder@gmail.com>
2024-06-01 21:51:28 +0200

    remove tests (moved back later)

--- tests/testthat/test_get_estimates_matrix.R
+++
@@ -1,6 +0,0 @@
-cor_est <- get_estimates(cor(iris[,1:4]))
-
-test_that("get_estimates works for correlations", {
-  expect_equal(as.vector(cor_est$estimate),
-               as.vector(cor(iris[,1:4])))
-  })
