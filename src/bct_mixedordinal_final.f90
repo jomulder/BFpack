1653a5ba0b23c2d74c8546d419f26af3acd00741
jomulder <jomulder@gmail.com>
2024-06-01 22:58:51 +0200

    test 10

--- src/bct_mixedordinal_final.f90
+++ src/bct_mixedordinal_final.f90
@@ -100,6 +100,7 @@ subroutine estimate_bct_ordinal(postZmean, postZcov, P, numcorr, K, numG, BHat, 
     !write(*,*)'end'
 
     !test write
+    Ccan = 1
     CDrawsStore(1,1,:,:) = Ccan(:,:)
 
 contains
