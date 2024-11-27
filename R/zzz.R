10b7a453c31942599fa5d8a725650cbcc0d79c28
jomulder <jomulder@gmail.com>
2024-11-27 13:28:00 +0100

    edit zzz

--- R/zzz.R
+++ R/zzz.R
@@ -6,11 +6,11 @@
   packageStartupMessage(
     "\n",
     "This is ", paste(pkgname, version),".", "\n",
-    "Updates on defaults:","\n",
+    "Updates on default settings:","\n",
     "- For standard (exploratory) tests, the default prior probability for a zero, negative,", "\n",
     "and positive effect are 0.5, 0.25, and 0.25, respectively. The previous default was 1/3 for each","\n",
     "hypothesis. Changing these prior probabilities can be done using the argument 'prior.hyp.explo'.", "\n",
-    "- For linear regression, ANOVA, t-tests, the fractional Bayes factor ('FBF') is now the default","\n",
+    "- For linear regression, ANOVA, t-tests, the fractional Bayes factor ('FBF') is the new default.","\n",
     "To change this to the adjusted fractional Bayes factor (the previous default), users can set","\n",
     "the argument: BF.type='AFBF'."
   )
